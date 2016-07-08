78e52e032296c4c72db8b1152f632e0be5630eae
Ashton Baker <ashtonsb@umich.edu>
2016-07-07 21:40:22 -0400

    got the stupid fucking eggs working, but recheck the scaling of time

--- daytoday.R
+++ daytoday.R
@@ -7,7 +7,7 @@ library(reshape2)
 stopifnot(packageVersion("pomp")>="1.4.9")
 
 # Import and organize the data for the model
-read.csv("./data.csv") %>%subset(weeks <= 40, select=c(weeks,rep,L_obs,P_obs,A_obs)) -> dat
+read.csv("./data.csv") %>%subset(weeks <= 40, select=c(weeks,rep,E_obs,L_obs,P_obs,A_obs)) -> dat
 
 dat %>%
   melt(id=c("weeks","rep")) %>%
@@ -16,40 +16,75 @@ dat %>%
 pomp(
   data = subset(dat, rep==2),
   times="weeks", t0=0,
-  globals = "int lstages = 14, pstages = 14, astages = 1, L_0 = 250, P_0 = 5, A_0 = 100;",
+  obsnames = c("E_obs", "L_obs", "P_obs", "A_obs"),
+  statenames = c(sprintf("E%d",1:14),sprintf("L%d",1:14),sprintf("P%d",1:14),"A"),
+  paramnames = c("b", "cea", "cel", "cpa", "mu_A", "mu_L", "gamma_L", "sigma_1", "sigma_2", "sigma_3"),
+  globals = "int estages = 14, lstages = 14, pstages = 14, astages = 1, L_0 = 250, P_0 = 5, A_0 = 100;",
   initializer=Csnippet("
-                      double mu_P = cpa * A;
+                      double *E = &E1;
                       double *L = &L1;
                       double *P = &P1;
                       double L_rate[100], P_rate[100] = {0};
-                      
-                      int k;
-                      for (k = 0; k < lstages; k++) L_rate[k] = pow(gamma_L / (gamma_L + mu_L), k);
-                      for (k = 0; k < pstages; k++) P_rate[k] = pow(gamma_P / (gamma_P + mu_P), k);
 
+                      int k;
+                      double L_tot = 0;
+                      for (k = 0; k < lstages; k++) L_tot += L[k];
+                      
+                      double gamma_E = exp((-0.01731 * L_tot - 0.01310 * A) * 0.07142857);
+                      double mu_E = 1 - gamma_E; //pow((1 - exp(-0.01731 * L_tot - 0.01310 * A)),0.07142857);
+                      double gamma_P = exp(-0.004619 * A / 14.0);
+                      double mu_P = 1 - gamma_P;//pow((1 - exp(-0.004619 * A)),1.0/14.0);
+                      
+                      for (k = 0; k < lstages; k++) L_rate[k] = pow(gamma_L, k);
+                      for (k = 0; k < pstages; k++) P_rate[k] = pow(gamma_P, k);
+
+                      for (k = 0; k < estages; k++) E[k] = 0;
                       reulermultinom(lstages, L_0, &L_rate[0], 1, &L[0]);
                       reulermultinom(pstages, P_0, &P_rate[0], 1, &P[0]);
                       A = 100;"),
-  statenames = c(sprintf("L%d",1:14),sprintf("P%d",1:14),"A"),
   rprocess=discrete.time.sim(
     step.fun=Csnippet('
+                      double *E = &E1;
                       double *L = &L1;
                       double *P = &P1;
-                      double rate[2], ltrans[28], ptrans[28], adeath;
+                      double rate[2], etrans[28], ltrans[28], ptrans[28], adeath;
                       int k;
-                      double eggs = rpois(b*A*dt/2);
+                      double L_tot = 0;
+                      for (k = 0; k < lstages; k++) L_tot += L[k];
+                      double gamma_E = exp((-0.01731 * L_tot - 0.01310 * A) * 0.07142857);
+                      double mu_E = 1 - gamma_E;//pow((1 - exp(-0.01731 * L_tot - 0.01310 * A)),0.07142857);
+                      double gamma_P = exp(-0.004619 * A / 14.0);
+                      double mu_P = 1 -gamma_P;//pow((1 - exp(-0.004619 * A)),1.0/14.0);
+                      
+                      // Calculate who goes where
+                      for (k = 0; k < estages; k++) {
+                        rate[0] = gamma_E;
+                        rate[1] = mu_E;
+                        reulermultinom(2,E[k],&rate[0],1,&etrans[2*k]);
+                      }
+
                       for (k = 0; k < lstages; k++) {
-                        rate[0] = gamma_L*lstages;
+                        rate[0] = gamma_L;
                         rate[1] = mu_L;
-                        reulermultinom(2,L[k],&rate[0],dt/2,&ltrans[2*k]);
+                        reulermultinom(2,L[k],&rate[0],1,&ltrans[2*k]);
                       }
+
                       for (k = 0; k < pstages; k++) {
-                        rate[0] = gamma_P*pstages;
-                        rate[1] = cpa*A;
-                        reulermultinom(2,P[k],&rate[0],dt/2,&ptrans[2*k]);
+                        rate[0] = gamma_P;
+                        rate[1] = mu_P;
+                        reulermultinom(2,P[k],&rate[0],1,&ptrans[2*k]);
                       }
-                      reulermultinom(1,A,&mu_A,dt/2,&adeath);
-                      L[0] += eggs;
+
+                      reulermultinom(1,A,&mu_A,1,&adeath);
+
+                      // Bookkeeping
+                      for (k = 0; k < estages; k++) {
+                        E[k] -= (etrans[2*k]+etrans[2*k+1]);
+                        E[k+1] += etrans[2*k]; // E[estages] == L[0]!!
+                      }
+                      
+                      E[0] += rpois(b*A);
+
                       for (k = 0; k < lstages; k++) {
                         L[k] -= (ltrans[2*k]+ltrans[2*k+1]);
                         L[k+1] += ltrans[2*k]; // L[lstages] == P[0]!!
@@ -62,31 +97,18 @@ pomp(
                      '),
     delta.t=1/7),
   rmeasure=Csnippet("
+                    const double *E = &E1;
                     const double *L = &L1;
                     const double *P = &P1;
                     int k;
-                    int L_tot = 0, P_tot = 0;
-                    for (k = 0; k < lstages; k++) L_tot += L[k];
-                    for (k = 0; k < pstages; k++) P_tot += P[k];
-
+                    double E_tot = 0, L_tot = 0, P_tot = 0;
+                    for (k = 0; k < estages; k++){ E_tot += E[k]; }
+                    for (k = 0; k < lstages; k++){ L_tot += L[k]; }
+                    for (k = 0; k < pstages; k++){ P_tot += P[k]; }
+                    
+                    E_obs = E_tot;
                     L_obs = L_tot;
                     P_obs = P_tot;
                     A_obs = A;"),
-  dmeasure=Csnippet("
-                    const double *L = &L1;
-                    const double *P = &P1;
-                    int k, L_tot, P_tot;
-                    for (k = 0; L_tot = 0; k < lstages, k++) L_tot += L[k];
-                    for (k = 0; P_tot = 0; k < pstages, k++) P_tot += P[k];
-
-                    double eps = 0.000001;
-                    if((abs(L_obs - L_tot) > eps) ||
-                    (abs(P_obs - P_tot) > eps) ||
-                    (abs(A_obs - A) > eps)) {
-                    lik = 0;
-                    } else {
-                    lik = 1;
-                    }"),
-  paramnames = c("b", "cea", "cel", "cpa", "mu_A", "mu_L", "gamma_L", "gamma_P", "sigma_1", "sigma_2", "sigma_3"),
-  params = c("b"=10.45, "cea"=0.01310, "cel"=0.01731, "cpa"=0.004619, "mu_A"=0.007629,"mu_L"=0.2,"gamma_L"=0.1,"gamma_P"=0.1,"sigma_1"=1.621, "sigma_2"=0.7375, "sigma_3"=0.01212)) -> model
+  params = c("b"=0.7464286, "cea"=0.01310, "cel"=0.01731, "cpa"=0.004619, "mu_A"=0.007629,"mu_L"=0.015812,"gamma_L"=0.984188,"sigma_1"=1.621, "sigma_2"=0.7375, "sigma_3"=0.01212)) -> model
 
